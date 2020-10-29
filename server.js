69e78f05d1a1a26f59af97e9128982ce0ebf932e
Nadav <nadavro1@gmail.com>
2020-10-29 16:50:34 +0200

    commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const connectDB= require('./config/db') 
 const app = express();
+const path= require('path')
 
 connectDB();
 
@@ -10,6 +11,16 @@ app.use(express.json({extended:false}))
 app.use('/api/users',require('./Routes/API/users'));
 app.use('/api/auth',require('./Routes/API/auth'));
 app.use('/api/patients',require('./Routes/API/patients'));
+
+if(process.env.NODE_ENV === 'production'){
+    console.log("using production")
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    });
+}
 
 const port= process.env.PORT || 5000
 app.listen(port,()=>{console.log(`Listening on port ${port}`)})
